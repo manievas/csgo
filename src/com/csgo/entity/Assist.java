03c874eebf648f79efbd6c4b8a2987da309b0aa4
manievas <mtnievas@gmail.com>
2022-06-30 20:27:33 -0300

    Update 30.06.2022

--- src/com/csgo/entity/Assist.java
+++ src/com/csgo/entity/Assist.java
@@ -8,7 +8,7 @@ public class Assist extends Event{
 	public Assist(int value, Round round) {
 		super(value, round);		
 	}
-	
+	 
     @Override
     public String toString() {
         return "Event: Assist";
